1d2e61ca1437d743ab9bcd2b008c67ebbe31b467
rochesebastien <sebastienroche230@gmail.com>
2023-12-28 03:07:44 +0100

    add : updating vercel crons for test

--- index.js
+++ index.js
@@ -29,8 +29,10 @@ app.use('/words/', WordRouter);
 
 // Putting the API on port 3000
 app.listen(process.env.API_PORT || 3000, () => {
+    console.log('------');
     console.log('\x1B[45m Welcome to \x1B[1m French Words API v1 ! \x1b[0m  ');
     console.log(`API is running on port \x1B[42m\x1B[31m ${process.env.API_PORT} \x1B[0m \x1B[0m `);
+    console.log('------');
 })
 
 // Export the Express API
